07f14388c0bbb865fdbd1fb347c4042fa558d878
Pratik <pratikpatil8560@gmail.com>
2023-06-04 20:48:14 +0530

    removed footer line

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -77,8 +77,8 @@ const Home = () => {
             </div>
             <footer>
                 <h4>
-                    Built with 💛&nbsp;by &nbsp;
-                    <a href="https://github.com/pratikjpatil">Pratik</a>
+                    {/* Built with 💛&nbsp;by &nbsp;
+                    <a href="https://github.com/pratikjpatil">Pratik</a> */}
                 </h4>
             </footer>
         </div>
